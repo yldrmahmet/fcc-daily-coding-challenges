3a7d1dee31045064160034046a87d63ba1cd9264
yldrmahmet <ay689089@gmail.com>
2025-09-10 20:52:05 +0300

    Add comments to clarify logic for finding differences in arrayDiff function

--- 09.10.25 - Array Diff/solution.js
+++ 09.10.25 - Array Diff/solution.js
@@ -1,6 +1,7 @@
 function arrayDiff(arr1, arr2) {
   const arr4 = [];
 
+  // ortak olanları bul ve arr4 arrayinde topla
   for (let i = 0; i < arr1.length; i++) {
     for (let j = 0; j < arr2.length; j++) {
       if (arr1[i] === arr2[j]) {
@@ -11,17 +12,21 @@ function arrayDiff(arr1, arr2) {
 
   const arr5 = [];
 
+  // arr1 deki farklı olanlar
   for (let i = 0; i < arr1.length; i++) {
     if (!arr4.includes(arr1[i])) {
       arr5.push(arr1[i]);
     }
   }
 
+  // arr2 deki farklı olanlar
   for (let i = 0; i < arr2.length; i++) {
     if (!arr4.includes(arr2[i])) {
       arr5.push(arr2[i]);
     }
   }
+
+  // arr5 = arr1 ve arr2'deki farklı olanların toplamı
 
   return arr5.sort();
 }
